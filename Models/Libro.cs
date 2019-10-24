3f07ce7e473c55104c0eb0df8b473d6292f0b02b
Bidkar Aragon Cardenas <bidkar@gmail.com>
2019-10-24 08:33:20 -0600

    Proyecto biblioteca con Mysql

---
+++ Models/Libro.cs
@@ -0,0 +1,7 @@
+namespace Tercer_Parcial.Models
+{
+    public class Libro
+    {
+        
+    }
+}
---
+++ Models/MysqlDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Tercer_Parcial.Models
+{
+    public class MysqlDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;user=admin_library;password=123;port=3306;dbname=library");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
