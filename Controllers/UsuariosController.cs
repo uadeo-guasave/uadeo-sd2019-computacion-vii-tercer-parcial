3d90d60dd0e8250c8a92231856639908d24813eb
Bidkar Aragon Cardenas <bidkar@gmail.com>
2019-11-13 08:20:37 -0700

    Controlador de usuarios

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Tercer_Parcial.Models;
+
+namespace Tercer_Parcial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        // GET api/usuarios
+        [HttpGet]
+        public ActionResult Get()
+        {
+            using (var db = new MysqlDbContext())
+            {
+                var usuarios = db.Usuarios.ToList();
+                return Ok(usuarios);
+            }
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -14,10 +14,10 @@ namespace Tercer_Parcial
     {
         public static void Main(string[] args)
         {
-            using (var db = new Models.MysqlDbContext())
-            {
-                db.Database.EnsureCreated();
-            }
+            // using (var db = new Models.MysqlDbContext())
+            // {
+            //     db.Database.EnsureCreated();
+            // }
             CreateWebHostBuilder(args).Build().Run();
         }
 
